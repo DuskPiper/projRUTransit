3bc30407f6fca87fcb622e2d17f71bb58c328b7a
Weitian <weitian.li@rutgers.edu>
2018-12-09 17:36:39 -0500

    Update SettingsActivity.java

--- app/src/main/java/com/mobileappeng/threegorgeous/projrutransit/SettingsActivity.java
+++ app/src/main/java/com/mobileappeng/threegorgeous/projrutransit/SettingsActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
